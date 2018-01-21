bdae1f9f8d79b3f437abdfd29d60ffc32bf0da63
Nishant Hooda <nishant.h@hotmail.com>
2018-01-20 23:42:36 -0500

    adding calculator files

---
+++ calculator/calculator/CalculatorBrain.swift
@@ -0,0 +1,75 @@
+//
+//  Calculator Brain.swift
+//  calculator
+//
+//  Created by Nishant Hooda on 2017-12-22.
+//  Copyright © 2017 Nishant Hooda. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    
+    private var accumulator = 0.0
+    
+    func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    private var operations : [String: Operation] = [
+        "π": Operation.constant(M_PI),
+        "e": Operation.constant(M_E),
+        "±": Operation.unaryOperation({ -$0 }),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "×": Operation.binaryOperation({$0 * $1}),
+        "÷": Operation.binaryOperation({$0 / $1}),
+        "+": Operation.binaryOperation({$0 + $1}),
+        "−": Operation.binaryOperation({$0 - $1}),
+        "=": Operation.equals
+    ]
+    
+    enum Operation{
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    func performOperation(_ symbol: String){
+        if let operation = operations[symbol]{
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                accumulator = function(accumulator)
+            case .binaryOperation(let function):
+                executeBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryOperation: function, firstOperand: accumulator)
+            case .equals:
+                executeBinaryOperation()
+            }
+        }
+    }
+    
+    private func executeBinaryOperation(){
+        if pending != nil{
+            accumulator = pending!.binaryOperation(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryOperation: (Double, Double) ->Double
+        var firstOperand: Double
+    }
+    
+    var result: Double{
+        get{
+            return accumulator
+        }
+    }
+}
+
---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Nishant Hooda on 2017-12-19.
+//  Copyright © 2017 Nishant Hooda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var display: UILabel!
+    
+    @IBOutlet weak var stack0: UIStackView!
+    @IBOutlet weak var stack1: UIStackView!
+    @IBOutlet weak var stack2: UIStackView!
+    @IBOutlet weak var stack3: UIStackView!
+    @IBOutlet weak var stack4: UIStackView!
+    
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue : Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let methematicalSymbol = sender.currentTitle{
+            brain.performOperation(methematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    override func willTransition(to newCollection: UITraitCollection,
+                                 with coordinator:UIViewControllerTransitionCoordinator) {
+        
+        super.willTransition(to: newCollection,
+                             with: coordinator)
+        configureView(newCollection.verticalSizeClass)
+    }
+    
+    private func configureView(_ verticalSizeClass: UIUserInterfaceSizeClass) {
+        if (verticalSizeClass == .compact)  {
+            stack1.insertArrangedSubview(multiplyButton, at: 0)
+            stack2.insertArrangedSubview(divideButton, at: 0)
+            stack3.insertArrangedSubview(plusButton, at: 0)
+            stack4.insertArrangedSubview(minusButton, at: 0)
+            stack0.isHidden = true
+        } else {
+            stack0.isHidden = false
+            stack0.addArrangedSubview(multiplyButton)
+            stack0.addArrangedSubview(divideButton)
+            stack0.addArrangedSubview(plusButton)
+            stack0.addArrangedSubview(minusButton)
+            
+        }
+    }
+    
+}
